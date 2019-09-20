f532c43e09c118da5cc4866372bc898190a7b572
Duong Hong Tuan - TuanDH - VS2015 <42463371+ktwiz@users.noreply.github.com>
2019-09-20 17:18:07 +0700

    add lib & code office 365

--- sb-admin-2.Web/Controllers/HomeController.cs
+++ sb-admin-2.Web/Controllers/HomeController.cs
@@ -3,14 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
 
 namespace sb_admin_2.Web.Controllers
 {
     public class HomeController : Controller
     {
+        [Authorize]
         public ActionResult Index()
         {
+            var userClaims = User.Identity as System.Security.Claims.ClaimsIdentity;
+
+            //You get the user’s first and last name below:
+            ViewBag.Name = userClaims?.FindFirst("name")?.Value;
+
+            // The 'preferred_username' claim can be used for showing the username
+            ViewBag.Username = userClaims?.FindFirst("preferred_username")?.Value;
+
+            // The subject/ NameIdentifier claim can be used to uniquely identify the user across the web
+            ViewBag.Subject = userClaims?.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier)?.Value;
+
+            // TenantId is the unique Tenant Id - which represents an organization in Azure AD
+            ViewBag.TenantId = userClaims?.FindFirst("http://schemas.microsoft.com/identity/claims/tenantid")?.Value;
+
             return View();
+        }
+
+        public ActionResult PageLogin()
+        {
+            return View();
+        }
+        public void SignOut()
+        {
+            HttpContext.GetOwinContext().Authentication.SignOut(
+                    OpenIdConnectAuthenticationDefaults.AuthenticationType,
+                    CookieAuthenticationDefaults.AuthenticationType);
+        }
+
+        public void SignIn()
+        {
+            if (!Request.IsAuthenticated)
+            {
+                HttpContext.GetOwinContext().Authentication.Challenge(
+                    new AuthenticationProperties { RedirectUri = "/" },
+                    OpenIdConnectAuthenticationDefaults.AuthenticationType);
+            }
         }
 
         public ActionResult FlotCharts()
